9bdde6d849e217b6036176a9d2c733c9d836038f
ChungHwan Lim <ljw6307@gmail.com>
2024-08-08 21:01:27 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,10 +1,3 @@
 def solution(array):
-    max_number = max(array)
     answer = []
-
-    for i in array:
-        if i == max_number:
-            answer.append(i)
-            answer.append(array.index(i))
-            
     return answer
